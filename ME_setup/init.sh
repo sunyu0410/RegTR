d7fc70a706c3a30120b10045161459f1f4cd7225
Yu Sun <sunyu0410@gmail.com>
2024-05-02 01:27:41 +0000

    Updated init.sh

--- ME_setup/init.sh
+++ ME_setup/init.sh
@@ -1,9 +1,16 @@
 apt update
 apt -y upgrade
-apt install libopenblas-dev nano unzip libxrender-dev
+apt install -y libopenblas-dev nano unzip libxrender-dev
 
 pip install wheels/MinkowskiEngine-0.5.4-cp310-cp310-linux_x86_64.whl
-python blinker-1.4/setup.py install # pip install wheels/blinker-1.4-py3-none-any.whl
-pip install wheels/pytorch3d-0.7.6-cp310-cp310-linux_x86_64.whl
 
+cd blinker-1.4
+python setup.py install # pip install wheels/blinker-1.4-py3-none-any.whl
+cd ..
+
+pip install wheels/pytorch3d-0.7.6-cp310-cp310-linux_x86_64.whl
 pip install coloredlogs easydict h5py GitPython nibabel numpy scipy open3d tensorboard vtk tensorboard
+
+git config --global user.email "sunyu0410@gmail.com"
+git config --global user.name "Yu Sun"
+
